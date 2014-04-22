1ea743f3278f3da290aa2aec87a2bf67fd9a205c
Rodolfo Arruda <rodolfoasantos@gmail.com>
2014-04-22 09:14:54 -0300

    Crete the dropbox client

--- core/services/dropbox.rb
+++ core/services/dropbox.rb
@@ -4,12 +4,12 @@ require 'yaml'
 module Services
   class Dropbox
 
-    DEFAULT_PATH = "/core/services/dropbox"
+    DEFAULT_PATH = "/core/services/dropbox/"
+    DEFAULT_UPLOAD_PATH = "/public/"
 
     def initialize
-      #TODO
-      #the user auth and test if already have a open session
-      #return nil
+      @flow = DropboxOAuth2FlowNoRedirect.new(load_keys['dropbox']['key'], load_keys['dropbox']['secret'])
+      @client = nil
     end
 
     def session
@@ -17,14 +17,30 @@ module Services
     end
 
     def authenticate_url
-      flow = DropboxOAuth2FlowNoRedirect.new(load_keys['dropbox']['key'], load_keys['dropbox']['secret'])
-      flow.start()
+      @flow.start()
+    end
+
+    def authorizate(code)
+      token, use_id = @flow.finish(code)
+      @client = DropboxClient.new(token)
+    end
+
+    def linked_account
+      @client.account_info() unless @client.nil?
+    end
+
+    def upload_file(filename, file)
+      begin
+        response = @client.put_file(DEFAULT_UPLOAD_PATH + filename, file)
+      raise DropboxError.new("error when try to upload the screenshot") if response["modified"].nil?
+      end
     end
 
     private
     def load_keys
-      @access ||= YAML.load(File.open("core/services/dropbox/keys.yml"))
+      @access ||= YAML.load(File.open(DEFAULT_PATH + "keys.yml"))
     end
 
   end
+
 end
